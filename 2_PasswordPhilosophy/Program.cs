761d14860479c83fb060adcbb0deeb9c2d71ee83
JulienBara <bara.julien@yahoo.fr>
2020-12-03 21:19:10 +0100

    dotnet new passwordphilosophy

---
+++ 2_PasswordPhilosophy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2_PasswordPhilosophy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
