a0b81eb3a610f507563ff00573a2fc94b1851670
JulienBara <bara.julien@yahoo.fr>
2020-12-03 23:27:05 +0100

    implement 3.2

--- 3_TobogganTrajectory/2/Program.cs
+++ 3_TobogganTrajectory/2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -7,25 +8,34 @@ namespace _3_TobogganTrajectory_2
     class Program
     {
         public const string InputFile = @".\input";
-        public const int rightPerTurn = 3;
-        public const int downPerTurn = 1;
+        public static List<Slope> slopes = new List<Slope>{
+            new Slope{ RightPerTurn = 1, DownPerTurn = 1 },
+            new Slope{ RightPerTurn = 3, DownPerTurn = 1 },
+            new Slope{ RightPerTurn = 5, DownPerTurn = 1 },
+            new Slope{ RightPerTurn = 7, DownPerTurn = 1 },
+            new Slope{ RightPerTurn = 1, DownPerTurn = 2 },
+        };
 
         static void Main(string[] args)
         {
             var forest = File
                 .ReadAllLines(InputFile);
 
-            var countTrees = 0;
-
-            for (int i = 0; i < forest.Count(); i++)
+            long multipleTrees = 1;
+            foreach (var slope in slopes)
             {
-                var rightPosition = i * rightPerTurn;
-                var downPosition = i * downPerTurn;
-                if(forest[downPosition][rightPosition % forest[downPosition].Count()] == '#')
-                    countTrees++;
+                var countTrees = 0;
+                for (int i = 0; i * slope.DownPerTurn < forest.Count(); i++)
+                {
+                    var rightPosition = i * slope.RightPerTurn;
+                    var downPosition = i * slope.DownPerTurn; 
+                    if(forest[downPosition][rightPosition % forest[downPosition].Count()] == '#')
+                        countTrees++;
+                }
+                multipleTrees *= countTrees;
             }
-
-            System.Console.WriteLine(countTrees);
+            
+            System.Console.WriteLine(multipleTrees);
         }
     }
 }
---
+++ 3_TobogganTrajectory/2/Slope.cs
@@ -0,0 +1,8 @@
+namespace _3_TobogganTrajectory_2
+{
+    public class Slope
+    {
+        public int RightPerTurn { get; set; }
+        public int DownPerTurn { get; set; }
+    }
+}
