c6d900efe9db455cd0d597fe25b2a8edbbf75c2c
JulienBara <bara.julien@yahoo.fr>
2020-12-03 22:39:33 +0100

    copy paste 3.1 to 3.2

---
+++ 3_TobogganTrajectory/2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace _3_TobogganTrajectory_2
+{
+    class Program
+    {
+        public const string InputFile = @".\input";
+        public const int rightPerTurn = 3;
+        public const int downPerTurn = 1;
+
+        static void Main(string[] args)
+        {
+            var forest = File
+                .ReadAllLines(InputFile);
+
+            var countTrees = 0;
+
+            for (int i = 0; i < forest.Count(); i++)
+            {
+                var rightPosition = i * rightPerTurn;
+                var downPosition = i * downPerTurn;
+                if(forest[downPosition][rightPosition % forest[downPosition].Count()] == '#')
+                    countTrees++;
+            }
+
+            System.Console.WriteLine(countTrees);
+        }
+    }
+}
