fa4391732d84cf8fde1c9e21be91ef87e5797094
JulienBara <bara.julien@yahoo.fr>
2020-12-04 07:25:19 +0100

    copy 4.1to 4.2

---
+++ 4_PassportProcessing/2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace _4_PassportProcessing_2
+{
+    class Program
+    {
+        public const string InputFile = @".\input";
+
+        static void Main(string[] args)
+        {
+            var validPassportsCount = File
+                .ReadAllText(InputFile)
+                .Split($"{Environment.NewLine}{Environment.NewLine}")
+                .Select(x => new Passport {String = x})
+                .Where(x => x.IsValid)
+                .Count()
+            ;
+
+            System.Console.WriteLine(validPassportsCount);
+        }
+    }
+}
