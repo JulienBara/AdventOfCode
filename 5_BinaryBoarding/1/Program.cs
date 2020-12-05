1a6b9ac3ab37fb5463a3352d730dd1b0982936c9
JulienBara <bara.julien@yahoo.fr>
2020-12-05 14:59:57 +0100

    5.1

---
+++ 5_BinaryBoarding/1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace _5_BinaryBoarding_1
+{
+    class Program
+    {
+        public const string InputFile = @".\input";
+
+        static void Main(string[] args)
+        {
+            var higherSeatId = File
+                .ReadLines(InputFile)
+                .Select(x => new Seat {String = x})
+                .OrderByDescending(x => x.SeatId)
+                .First()
+                .SeatId
+                ;
+
+            System.Console.WriteLine(higherSeatId);
+        }
+    }
+}
