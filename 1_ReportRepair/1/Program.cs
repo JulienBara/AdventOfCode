4ef15f57e07cf4d4c70974a954652ec46cd6afc5
JulienBara <bara.julien@yahoo.fr>
2020-12-06 11:33:47 +0100

    clean 1.2

--- 1_ReportRepair/1/Program.cs
+++ 1_ReportRepair/1/Program.cs
@@ -1,46 +1,37 @@
 ﻿using System.IO;
 using System.Linq;
 
-namespace _1_ReportRepair
+var inputFile = @".\input";
+var sumToFind = 2020;
+
+var numbers = File
+    .ReadAllLines(inputFile)
+    .Select(x => System.Int32.Parse(x));
+
+var lowerNumbersStack = numbers
+    .Where(x => x < sumToFind / 2)
+    .OrderByDescending(x => x)
+    .ToList();
+var higherNumbersStack = numbers
+    .Where(x => x > sumToFind / 2)
+    .OrderBy(x => x)
+    .ToList();
+
+while(lowerNumbersStack.Count > 0 && higherNumbersStack.Count > 0)
 {
-    class Program
+    var lowerNumber = lowerNumbersStack.First();
+    var higherNumber = higherNumbersStack.First();
+
+    switch (lowerNumber + higherNumber)
     {
-        public const string InputFile = @".\input";
-        public const int SumToFind = 2020;
-
-        static void Main(string[] args)
-        {
-            var numbers = File
-                .ReadAllLines(InputFile)
-                .Select(x => System.Int32.Parse(x));
-
-            var lowerNumbersStack = numbers
-                .Where(x => x < SumToFind / 2)
-                .OrderByDescending(x => x)
-                .ToList();
-            var higherNumbersStack = numbers
-                .Where(x => x > SumToFind / 2)
-                .OrderBy(x => x)
-                .ToList();
-
-            while(lowerNumbersStack.Count > 0 && higherNumbersStack.Count > 0)
-            {
-                var lowerNumber = lowerNumbersStack.First();
-                var higherNumber = higherNumbersStack.First();
-
-                switch (lowerNumber + higherNumber)
-                {
-                    case int sum when sum > SumToFind:
-                        lowerNumbersStack.RemoveAt(0);
-                        break;
-                    case int sum when sum < SumToFind:
-                        higherNumbersStack.RemoveAt(0);
-                        break;
-                    default:
-                        System.Console.WriteLine(lowerNumber * higherNumber);
-                        return;
-                }
-            }
-        }
+        case int sum when sum > sumToFind:
+            lowerNumbersStack.RemoveAt(0);
+            break;
+        case int sum when sum < sumToFind:
+            higherNumbersStack.RemoveAt(0);
+            break;
+        default:
+            System.Console.WriteLine(lowerNumber * higherNumber);
+            return;
     }
 }
