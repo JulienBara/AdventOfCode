2779ee41353927422aac3a06cc7bddbe93bd6358
JulienBara <bara.julien@yahoo.fr>
2020-12-06 11:30:30 +0100

    use top level statements

--- 6_CustomCustoms/1/Program.cs
+++ 6_CustomCustoms/1/Program.cs
@@ -1,23 +1,16 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using  _6_CustomCustoms_1;
 
-namespace _6_CustomCustoms_1
-{
-    class Program
-    {
-        public const string InputFile = @".\input";
+var inputFile = @".\input";
 
-        static void Main(string[] args)
-        {
-            var sumCountDifferentYes = File
-                .ReadAllText(InputFile)
-                .Split($"{Environment.NewLine}{Environment.NewLine}")
-                .Select(x => new Group {String = x})
-                .Sum(x => x.NumberOfDifferentYes)
-                ;
 
-            System.Console.WriteLine(sumCountDifferentYes);
-        }
-    }
-}
+var sumCountDifferentYes = File
+    .ReadAllText(inputFile)
+    .Split($"{Environment.NewLine}{Environment.NewLine}")
+    .Select(x => new Group {String = x})
+    .Sum(x => x.NumberOfDifferentYes)
+    ;
+
+System.Console.WriteLine(sumCountDifferentYes);
--- 6_CustomCustoms/2/Program.cs
+++ 6_CustomCustoms/2/Program.cs
@@ -1,23 +1,15 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using _6_CustomCustoms_2;
 
-namespace _6_CustomCustoms_2
-{
-    class Program
-    {
-        public const string InputFile = @".\input";
+var inputFile = @".\input";
 
-        static void Main(string[] args)
-        {
-            var sumCountDifferentYes = File
-                .ReadAllText(InputFile)
-                .Split($"{Environment.NewLine}{Environment.NewLine}")
-                .Select(x => new Group {String = x})
-                .Sum(x => x.NumberOfDifferentYes)
-                ;
+var sumCountDifferentYes = File
+    .ReadAllText(inputFile)
+    .Split($"{Environment.NewLine}{Environment.NewLine}")
+    .Select(x => new Group {String = x})
+    .Sum(x => x.NumberOfDifferentYes)
+    ;
 
-            System.Console.WriteLine(sumCountDifferentYes);
-        }
-    }
-}
+System.Console.WriteLine(sumCountDifferentYes);
