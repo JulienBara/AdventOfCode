babd9759d1b44676759a84c11c837a9aed9c2e27
JulienBara <bara.julien@yahoo.fr>
2020-12-08 21:46:09 +0100

    8.2

--- 8_HandheldHalting/2/Program.cs
+++ 8_HandheldHalting/2/Program.cs
@@ -1,52 +1,68 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
 var inputFile = @".\input";
 
-var instructions = File
+var lines = File
     .ReadAllLines(inputFile)
-    .Select(x => new 
-    {
-        Line = x,
-        Operation = x.Substring(0,3),
-        Argument = new 
-        {
-            Sign = x[4],
-            Value = int.Parse(x.Substring(5))
-        }
-    })
-    .ToArray();
+    .ToList();
 
-var nextInstructionIndex = 0;
-var accumulator = 0;
-var visitedIndexes = new bool[instructions.Length];
-
-while (! visitedIndexes[nextInstructionIndex])
+for (int i = 0; i < lines.Count; i++)
 {
-    visitedIndexes[nextInstructionIndex] = true;
+    var copiedLines = new List<string>(lines);
+    if(copiedLines[i].Substring(0,3) == "acc")
+        continue;
+    if(copiedLines[i].Substring(0,3) == "nop")
+        copiedLines[i] = copiedLines[i].Replace("nop", "jmp");
+    else
+        copiedLines[i] = copiedLines[i].Replace("jmp", "nop");
 
-    var instruction = instructions[nextInstructionIndex];
-    switch (instruction.Operation)
+    var instructions = copiedLines
+        .Select(x => new 
+        {
+            Line = x,
+            Operation = x.Substring(0,3),
+            Argument = new 
+            {
+                Sign = x[4],
+                Value = int.Parse(x.Substring(5))
+            }
+        })
+        .ToArray();
+
+    var nextInstructionIndex = 0;
+    var accumulator = 0;
+    var visitedIndexes = new bool[instructions.Length];
+
+    while (nextInstructionIndex != instructions.Count() && ! visitedIndexes[nextInstructionIndex])
     {
-        case "acc" when instruction.Argument.Sign == '+':
-            accumulator += instruction.Argument.Value;
-            nextInstructionIndex++;
-            break;
-        case "acc" when instruction.Argument.Sign == '-':
-            accumulator -= instruction.Argument.Value;
-            nextInstructionIndex++;
-            break;
-        case "jmp" when instruction.Argument.Sign == '+':
-            nextInstructionIndex += instruction.Argument.Value;
-            break;
-        case "jmp" when instruction.Argument.Sign == '-':
-            nextInstructionIndex -= instruction.Argument.Value;
-            break;
-        case "nop":
-            nextInstructionIndex++;
-            break;
-    }
-}
+        visitedIndexes[nextInstructionIndex] = true;
 
-Console.WriteLine(accumulator);
+        var instruction = instructions[nextInstructionIndex];
+        switch (instruction.Operation)
+        {
+            case "acc" when instruction.Argument.Sign == '+':
+                accumulator += instruction.Argument.Value;
+                nextInstructionIndex++;
+                break;
+            case "acc" when instruction.Argument.Sign == '-':
+                accumulator -= instruction.Argument.Value;
+                nextInstructionIndex++;
+                break;
+            case "jmp" when instruction.Argument.Sign == '+':
+                nextInstructionIndex += instruction.Argument.Value;
+                break;
+            case "jmp" when instruction.Argument.Sign == '-':
+                nextInstructionIndex -= instruction.Argument.Value;
+                break;
+            case "nop":
+                nextInstructionIndex++;
+                break;
+        }   
+    }
+
+    if(nextInstructionIndex == lines.Count)
+        Console.WriteLine(accumulator);
+}8.2
