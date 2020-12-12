815ccbc74d111d2b153a7ac54768ea2315ff8313
JulienBara <bara.julien@yahoo.fr>
2020-12-12 13:26:31 +0100

    9.1

--- 9_EncodingError/1/Program.cs
+++ 9_EncodingError/1/Program.cs
@@ -1,3 +1,26 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 
-Console.WriteLine("Hello World!");
+var inputFile = @".\input";
+var preambuleSize = 25;
+
+var numbers = File
+    .ReadAllLines(inputFile)
+    .Select(x => long.Parse(x))
+    .ToList();
+
+for (int i = preambuleSize; i < numbers.Count; i++)
+{
+    var isSum = false;
+    for (int j = Math.Max(0, i - preambuleSize); j < i; j++)
+    {
+        for (int k = j + 1; k < i; k++)
+        {
+            if(numbers[j] + numbers[k] == numbers[i])
+                isSum = true;
+        }
+    }
+    if(! isSum)
+        Console.WriteLine(numbers[i]);
+}
