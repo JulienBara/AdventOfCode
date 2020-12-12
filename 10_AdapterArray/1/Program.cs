541b57bcb0a0ae6186faa9f705dca50cee720497
JulienBara <bara.julien@yahoo.fr>
2020-12-12 17:50:31 +0100

    10.1

--- 10_AdapterArray/1/Program.cs
+++ 10_AdapterArray/1/Program.cs
@@ -1,4 +1,20 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 
+var inputFile = @".\input";
 
-Console.WriteLine("Hello World!");
+var aggregateAdapters = File
+    .ReadAllLines(inputFile)
+    .Select(x => int.Parse(x))
+    .OrderBy(x => x)
+    .Aggregate(
+        new { Previous = 0, Count1 = 0, Count3 = 0 },
+        (accu, current) => new 
+        { 
+            Previous = current, 
+            Count1 = accu.Count1 + (current - accu.Previous == 1 ? 1 : 0), 
+            Count3 = accu.Count3 + (current - accu.Previous == 3 ? 1 : 0), 
+        }); 
+
+Console.WriteLine(aggregateAdapters.Count1 * (aggregateAdapters.Count3 + 1));
