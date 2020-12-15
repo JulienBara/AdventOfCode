8e5fab06f7c0e11c11ac2989e1f819e3ed8e475f
JulienBara <bara.julien@yahoo.fr>
2020-12-15 20:49:10 +0100

    cp 15.1 to 15.2

---
+++ 15_RambunctiousRecitation/2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+var inputFile = @".\input";
+var stopCount = 2020;
+
+var startingNumbers = File
+    .ReadAllText(inputFile)
+    .Split(",")
+    .Select(x => long.Parse(x));
+
+var count = 0;
+var numbers = new List<long>();
+
+foreach (var startingNumber in startingNumbers)
+{
+    numbers.Add(startingNumber);
+    count++;
+}
+
+while (count < stopCount)
+{
+    var number = numbers.Last();
+    long nextNumber;
+    if(numbers.Where(x => x == number).Count() == 1){
+        nextNumber = 0;
+    }else{
+        nextNumber = numbers.Count() 
+            - (numbers.LastIndexOf(number, numbers.Count() - 2) + 1); // last tour number = last index + 1
+    }
+
+    numbers.Add(nextNumber);
+    count++;
+}
+
+Console.WriteLine(numbers.Last());
