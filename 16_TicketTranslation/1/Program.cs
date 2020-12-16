a5b8509b318b6a9e7888d3a3152b9073a42815d0
JulienBara <bara.julien@yahoo.fr>
2020-12-16 09:18:17 +0100

    16.1

--- 16_TicketTranslation/1/Program.cs
+++ 16_TicketTranslation/1/Program.cs
@@ -1,3 +1,40 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using _16_TicketTranslation_1;
 
-Console.WriteLine("Hello World!");
+var inputFile = @".\input";
+
+var input = File.ReadAllText(inputFile);
+
+var fields = input
+    .Split($"{Environment.NewLine}{Environment.NewLine}")[0]
+    .Split(Environment.NewLine)
+    .Select(x => new Field {String = x});
+
+var yourTicket = input
+    .Split($"{Environment.NewLine}{Environment.NewLine}")[1]
+    .Split(Environment.NewLine)[1]
+    .Split(",")
+    .Select(x => int.Parse(x));
+
+var nearbyTickets = input
+    .Split($"{Environment.NewLine}{Environment.NewLine}")[2]
+    .Split(Environment.NewLine)
+    .Skip(1)
+    .Select(x => x
+        .Split(",")
+        .Select(y => int.Parse(y)));
+
+var sum = 0;
+foreach (var nearbyTicket in nearbyTickets)
+{
+    foreach (var value in nearbyTicket)
+    {
+        if(! fields.Any(x => x.Validator(value)))
+            sum += value;
+    }
+}
+
+Console.WriteLine(sum);
