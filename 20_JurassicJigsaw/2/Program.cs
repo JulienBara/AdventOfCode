bcde3087de34c4a1877ec1ed31d5dcb26a5ae70a
JulienBara <bara.julien@yahoo.fr>
2020-12-20 17:57:53 +0100

    cp 20.1 20.2

---
+++ 20_JurassicJigsaw/2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using _20_JurassicJigsaw_2;
+
+var inputFile = @".\input";
+
+var tiles = File
+    .ReadAllText(inputFile)
+    .Split(Environment.NewLine + Environment.NewLine)
+    .Select(x => new Tile { String = x })
+    .ToList();
+
+var productCorner = tiles
+    .Where(x => x.IsCorner(tiles))
+    .Aggregate(
+        (long)1, 
+        (accu, tile) => accu *= long.Parse(tile.Id)
+    );
+
+Console.WriteLine(productCorner);
---
+++ 20_JurassicJigsaw/2/Tile.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _20_JurassicJigsaw_2
+{
+    public class Tile
+    {
+        public string String { get; set; }
+
+        public string Id => String
+            .Split(Environment.NewLine)
+            .First()
+            .Skip("Tile ".Length)
+            .SkipLast(1)
+            .Aggregate("", (s, c) => s += c);
+
+        public List<string> Square => String
+            .Split(Environment.NewLine)
+            .Skip(1)
+            .ToList();
+
+        public string NorthLine => Square.First();
+        public string SouthLine => Square
+            .Last()
+            .Reverse()
+            .Aggregate("", (s, c) => s += c);
+        public string WestLine => Square
+            .Select(x => x.First())
+            .Reverse()
+            .Aggregate("", (s, c) => s += c);         
+        public string EastLine => Square
+            .Select(x => x.Last())
+            .Aggregate("", (s, c) => s += c);
+
+        
+        public List<string> Lines => new List<string>
+            {
+                NorthLine,
+                EastLine,
+                SouthLine,
+                WestLine
+            };
+
+        public List<string> ReversedLines => Lines
+            .Select(x => x.Reverse().Aggregate("", (s, c) => s += c))
+            .ToList();
+
+        public List<string> AllLines => Lines
+            .Concat(ReversedLines)
+            .ToList();
+
+        public bool IsCorner(List<Tile> tiles)
+        {
+            var filteredTiles = tiles.Where(x => x.Id != Id);
+            var countMatchingBorders = Lines
+                .Where(line => filteredTiles
+                    .Any(filteredTile => filteredTile.AllLines.Contains(line))
+                )
+                .Count();
+            return countMatchingBorders == 2;
+        }
+    }
+}
