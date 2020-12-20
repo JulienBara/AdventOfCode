f2351f4051298b5fbe95745e28122c9409b63122
JulienBara <bara.julien@yahoo.fr>
2020-12-20 17:56:17 +0100

    20.1

--- 20_JurassicJigsaw/1/Program.cs
+++ 20_JurassicJigsaw/1/Program.cs
@@ -1,3 +1,21 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using _20_JurassicJigsaw_1;
 
-Console.WriteLine("Hello World!");
+var inputFile = @".\input";
+
+var tiles = File
+    .ReadAllText(inputFile)
+    .Split(Environment.NewLine + Environment.NewLine)
+    .Select(x => new Tile { String = x })
+    .ToList();
+
+var productCorner = tiles
+    .Where(x => x.IsCorner(tiles))
+    .Aggregate(
+        (long)1, 
+        (accu, tile) => accu *= long.Parse(tile.Id)
+    );
+
+Console.WriteLine(productCorner);
