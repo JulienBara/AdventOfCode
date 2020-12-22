148fbdadd1e89624c18d59b72edf47aa099607be
JulienBara <bara.julien@yahoo.fr>
2020-12-22 08:30:18 +0100

    cp 21.1 to 21.2

---
+++ 21_AllergenAssessment/2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.IO;
+using System.Linq;
+using _21_AllergenAssessment_2;
+
+var inputFile = @".\input";
+
+var foods = File
+    .ReadAllLines(inputFile)
+    .Select(x => new Food(x))
+    .ToList();
+
+var allergens = new Dictionary<string,List<string>>();
+
+foreach (var food in foods)
+{
+    foreach (var allergen in food.Allergens)
+    {
+        if(! allergens.ContainsKey(allergen)) allergens.Add(allergen, food.Ingredients);
+
+        allergens[allergen] = allergens[allergen].Intersect(food.Ingredients).ToList();
+    }
+}
+
+var ingredients = foods.SelectMany(x => x.Ingredients).Distinct().ToList();
+
+while(allergens.Count() > 0)
+{
+    var foundAllergen = allergens.First(x => x.Value.Count == 1);
+    var ingredient = foundAllergen.Value.First();
+    allergens.Remove(foundAllergen.Key);
+    ingredients.Remove(ingredient);
+    foreach (var allergen in allergens)
+    {
+        allergen.Value.Remove(ingredient);
+    }
+};
+
+var sum = foods.Sum(x => x.Ingredients.Where(y => ingredients.Contains(y)).Count());
+
+Console.WriteLine(sum);
