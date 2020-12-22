5ca09083cd95dc4fdb5431729dce596bd0a0c0e2
JulienBara <bara.julien@yahoo.fr>
2020-12-22 08:28:12 +0100

    21.1

---
+++ 21_AllergenAssessment/1/Food.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _21_AllergenAssessment_1
+{
+    public class Food
+    {
+        public Food(string s)
+        {
+            Ingredients = s.Split(" (contains ").First().Split(" ").ToList();
+            Allergens = s.Split(" (contains ").Last().TrimEnd(')').Split(", ").ToList();
+        }
+
+        public List<string> Ingredients { get; set; }
+        public List<string> Allergens { get; set; }
+    }
+}
