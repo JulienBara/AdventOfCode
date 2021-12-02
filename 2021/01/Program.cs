314b10d9cb640cdce866c04240b9a4900af9aa0c
Julien Bara <bara.julien@yahoo.fr>
2021-12-02 07:44:02 +0100

    1-1

---
+++ 2021/01/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using System.Linq;
+
+var inputFile = @".\input";
+
+var numbers = File
+    .ReadAllLines(inputFile)
+    .Select(x => System.Int32.Parse(x));
+
+var increasesCount = 0;
+int? previous = null;
+
+foreach (var number in numbers)
+{
+    if (previous < number) { increasesCount++; }
+    previous = number;
+}
+
+System.Console.WriteLine(increasesCount);
