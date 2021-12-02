19653f54177ded65074c504db2c003a00a88d228
Julien Bara <bara.julien@yahoo.fr>
2021-12-02 08:43:52 +0100

    Revert "rename folder"
    This reverts commit 90c63ad90536f0a0b658ecfae6c7350b6973a1c7.

---
+++ 2021/2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+var inputFile = Path.Combine(".", "input");
+
+var position = File
+    .ReadAllLines(inputFile)
+    .Select(x => x.Split())
+    .Select(x => new Command(x[0], Int32.Parse(x[1])))
+    .Aggregate(
+        new { Horizontal = 0, Depth = 0, Aim = 0 },
+        (position, command) => command switch
+        {
+            Command { Direction: "forward" } => new { Horizontal = position.Horizontal + command.Unit, Depth = position.Depth + position.Aim * command.Unit, Aim = position.Aim },
+            Command { Direction: "down" } => new { Horizontal = position.Horizontal, Depth = position.Depth, Aim = position.Aim + command.Unit },
+            Command { Direction: "up" } => new { Horizontal = position.Horizontal, Depth = position.Depth, Aim = position.Aim - command.Unit }
+        }
+    );
+
+System.Console.WriteLine(position.Horizontal * position.Depth);
+
+public record Command(string Direction, int Unit);
