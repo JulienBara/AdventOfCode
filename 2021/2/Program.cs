12ead67ef2c1dc41efd16207da3cac995267a169
Julien Bara <bara.julien@yahoo.fr>
2021-12-02 08:43:00 +0100

    2-2

--- 2021/2/Program.cs
+++ 2021/2/Program.cs
@@ -10,12 +10,12 @@ var position = File
     .Select(x => x.Split())
     .Select(x => new Command(x[0], Int32.Parse(x[1])))
     .Aggregate(
-        new { Horizontal = 0, Depth = 0 },
+        new { Horizontal = 0, Depth = 0, Aim = 0 },
         (position, command) => command switch
         {
-            Command { Direction: "forward" } => new { Horizontal = position.Horizontal + command.Unit, Depth = position.Depth },
-            Command { Direction: "down" } => new { Horizontal = position.Horizontal, Depth = position.Depth + command.Unit },
-            Command { Direction: "up" } => new { Horizontal = position.Horizontal, Depth = position.Depth - command.Unit }
+            Command { Direction: "forward" } => new { Horizontal = position.Horizontal + command.Unit, Depth = position.Depth + position.Aim * command.Unit, Aim = position.Aim },
+            Command { Direction: "down" } => new { Horizontal = position.Horizontal, Depth = position.Depth, Aim = position.Aim + command.Unit },
+            Command { Direction: "up" } => new { Horizontal = position.Horizontal, Depth = position.Depth, Aim = position.Aim - command.Unit }
         }
     );
 
