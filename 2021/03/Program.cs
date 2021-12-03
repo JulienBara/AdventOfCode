3821f9b5ea1b0375d80adc810617ed1a63aac9b7
Julien Bara <bara.julien@yahoo.fr>
2021-12-03 08:43:59 +0100

    3-2

--- 2021/03/Program.cs
+++ 2021/03/Program.cs
@@ -2,23 +2,33 @@
 using System.IO;
 using System.Linq;
 
+
 var inputFile = Path.Combine(".", "input");
 var lines = File.ReadAllLines(inputFile);
 
-var oneCounts = new int[lines[0].Length];
-foreach (var line in lines)
+
+var validOxygens = lines.ToList();
+for (int i = 0; i < lines[0].Length && validOxygens.Count > 1; i++)
 {
-    for (int i = 0; i < line.Count(); i++)
-    {
-        if (line[i] == '1') oneCounts[i]++;
-    }
+    var treshold = validOxygens.Count / 2.0;
+    var oneCount = validOxygens.Where(x => x[i] == '1').Count();
+    var mostCommonBit = oneCount >= treshold ? '1' : '0';
+    validOxygens = validOxygens.Where(x => x[i] == mostCommonBit).ToList();
 }
-
-var treshold = lines.Length / 2;
-var gamma = String.Join("", oneCounts.Select(x => x > treshold ? "1" : "0"));
-var epsilon = String.Join("", oneCounts.Select(x => x > treshold ? "0" : "1"));
-
+var gamma = validOxygens.First();
 int gammaRate = Convert.ToInt32(gamma, 2);
+
+
+var validCo2 = lines.ToList();
+for (int i = 0; i < lines[0].Length && validCo2.Count > 1; i++)
+{
+    var treshold = validCo2.Count / 2.0;
+    var oneCount = validCo2.Where(x => x[i] == '1').Count();
+    var mostCommonBit = oneCount >= treshold ? '1' : '0';
+    validCo2 = validCo2.Where(x => x[i] != mostCommonBit).ToList();
+}
+var epsilon = validCo2.First();
 int epsilonRate = Convert.ToInt32(epsilon, 2);
+
 
 Console.WriteLine(gammaRate * epsilonRate);
