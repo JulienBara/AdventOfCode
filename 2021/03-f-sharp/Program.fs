eb314b3ca6bac7a5c54301507a91f3580b81447c
Julien Bara <bara.julien@yahoo.fr>
2021-12-03 22:50:43 +0100

    simplify

--- 2021/03-f-sharp/Program.fs
+++ 2021/03-f-sharp/Program.fs
@@ -1,17 +1,18 @@
 ﻿open System.IO
 
 
-let parseLine (s: string) = Seq.toArray s
+let parseLine = Seq.toArray
 
 let initBalances (lines: seq<char []>) = Array.zeroCreate (Array.length (Seq.head lines))
 
+// positive balance means more 1s than 0s
 let computeBalance _ (balance: int) (c: char) = 
     match c with 
     | '0' -> balance - 1
     | '1' -> balance + 1
 let computeBalances = Array.mapi2 computeBalance
-let convertToMostCommonBit (balance: int) = if balance >= 0 then '1' else '0'
-let convertToMostCommonBits = Array.map convertToMostCommonBit
+
+let convertToMostCommonBits = Array.map (fun (balance: int) -> if balance >= 0 then '1' else '0')
 
 let convertToInt (s: char[]) = System.Convert.ToInt32(s |> System.String, 2)
 let mirror = Array.map (fun (x: char) -> if x = '1' then '0' else '1')
