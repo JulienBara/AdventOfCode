c1c3564f9e359de200637eebd905d6810ad14903
Julien Bara <bara.julien@yahoo.fr>
2021-12-06 07:54:08 +0100

    6-1

---
+++ 2021/06/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+var inputFile = Path.Combine(".", "input");
+
+var fishes = File
+    .ReadAllText(inputFile)
+    .Split(",")
+    .Select(x => Int32.Parse(x))
+    .ToList();
+
+for (int i = 0; i < 80; i++)
+{
+    var nextFishes = new List<int>();
+    foreach (var fish in fishes)
+    {
+        if (fish == 0)
+        {
+            nextFishes.Add(8);
+            nextFishes.Add(6);
+            continue;
+        }
+
+        nextFishes.Add(fish - 1);
+    }
+    fishes = nextFishes;
+}
+
+Console.WriteLine(fishes.Count);
