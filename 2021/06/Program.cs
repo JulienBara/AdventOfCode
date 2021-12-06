5832c9c11f2a6c0f6f347bb0dc5df982726f038e
Julien Bara <bara.julien@yahoo.fr>
2021-12-06 09:02:03 +0100

    6-2

--- 2021/06/Program.cs
+++ 2021/06/Program.cs
@@ -5,27 +5,30 @@ using System.Linq;
 
 var inputFile = Path.Combine(".", "input");
 
-var fishes = File
+var fishCountPerAge = File
     .ReadAllText(inputFile)
     .Split(",")
     .Select(x => Int32.Parse(x))
-    .ToList();
+    .GroupBy(x => x)
+    .ToDictionary(x => x.Key, x => (long)x.Count());
 
-for (int i = 0; i < 80; i++)
+for (int i = 0; i < 256; i++)
 {
-    var nextFishes = new List<int>();
-    foreach (var fish in fishes)
-    {
-        if (fish == 0)
-        {
-            nextFishes.Add(8);
-            nextFishes.Add(6);
-            continue;
-        }
+    fishCountPerAge = Enumerable.Range(0, 9)
+        .ToDictionary(
+            x => x,
+            x =>
+            {
+                if (x == 8) { return fishCountPerAge.ContainsKey(0) ? fishCountPerAge[0] : 0; }
+                if (x == 6)
+                {
+                    return (fishCountPerAge.ContainsKey(0) ? fishCountPerAge[0] : 0)
+                    + (fishCountPerAge.ContainsKey(7) ? fishCountPerAge[7] : 0);
+                }
 
-        nextFishes.Add(fish - 1);
-    }
-    fishes = nextFishes;
+                return fishCountPerAge.ContainsKey(x + 1) ? fishCountPerAge[x + 1] : 0;
+            }
+        );
 }
 
-Console.WriteLine(fishes.Count);
+Console.WriteLine(fishCountPerAge.Values.Sum());
