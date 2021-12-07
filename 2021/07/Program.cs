d3fe4a9766ab1cca5cf2f083b72753f8fab3fe8c
Julien Bara <bara.julien@yahoo.fr>
2021-12-07 08:22:20 +0100

    7-2

--- 2021/07/Program.cs
+++ 2021/07/Program.cs
@@ -10,9 +10,13 @@ var positions = File
     .Split(",")
     .Select(x => Int32.Parse(x));
 
+var distancesCosts = Enumerable.Range(0, positions.Max() - positions.Min() + 1)
+    .Select(distance => Enumerable.Range(0, distance + 1).Sum())
+    .ToArray();
+
 var min = Enumerable.Range(positions.Min(), positions.Max() + 1)
     .Select(point => positions
-        .Select(position => Math.Abs(position - point))
+        .Select(position => distancesCosts[Math.Abs(position - point)])
         .Sum()
     )
     .Min();
