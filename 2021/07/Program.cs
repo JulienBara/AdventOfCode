ea2a39f456e79884b00bdc950e1663f5ad60b6c3
Julien Bara <bara.julien@yahoo.fr>
2021-12-07 08:16:11 +0100

    7-1

---
+++ 2021/07/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+var inputFile = Path.Combine(".", "input");
+
+var positions = File
+    .ReadAllText(inputFile)
+    .Split(",")
+    .Select(x => Int32.Parse(x));
+
+var min = Enumerable.Range(positions.Min(), positions.Max() + 1)
+    .Select(point => positions
+        .Select(position => Math.Abs(position - point))
+        .Sum()
+    )
+    .Min();
+
+Console.WriteLine(min);
