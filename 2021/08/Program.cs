3d0a5544bb72d672722a37d0d456b183e1e7feaf
Julien Bara <bara.julien@yahoo.fr>
2021-12-08 08:11:27 +0100

    8-1

---
+++ 2021/08/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+var inputFile = Path.Combine(".", "input");
+
+var result = File
+    .ReadAllLines(inputFile)
+    .Select(x => x
+        .Split(" | ")
+        [1]
+        .Split(" ")
+        .Where(x =>
+            x.Length == 2 // 1
+            || x.Length == 4 // 4
+            || x.Length == 3 // 7
+            || x.Length == 7 // 8
+        )
+        .Count()
+    ).Sum();
+
+Console.WriteLine(result);
