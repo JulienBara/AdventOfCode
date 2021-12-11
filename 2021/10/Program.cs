de27786d2e53a93fb6eada298cfdfe94046e5bc8
Julien Bara <bara.julien@yahoo.fr>
2021-12-11 13:50:28 +0100

    10-1

---
+++ 2021/10/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+var inputFile = Path.Combine(".", "input");
+
+var openingClosingChunks = new Dictionary<char, char>
+{
+    ['('] = ')',
+    ['['] = ']',
+    ['{'] = '}',
+    ['<'] = '>'
+};
+
+var chunkScores = new Dictionary<char, int>
+{
+    [')'] = 3,
+    [']'] = 57,
+    ['}'] = 1197,
+    ['>'] = 25137
+};
+
+var score = File
+    .ReadAllLines(inputFile)
+    .Aggregate(
+        0,
+        (accu, line) =>
+        {
+            var stack = new Stack<char>();
+            foreach (var c in line)
+            {
+                if (openingClosingChunks.ContainsKey(c))
+                {
+                    stack.Push(c);
+                    continue;
+                }
+
+                var topChar = stack.Pop();
+                if (openingClosingChunks[topChar] != c)
+                    return accu + chunkScores[c];
+            }
+
+            return accu;
+        });
+
+Console.WriteLine(score);
