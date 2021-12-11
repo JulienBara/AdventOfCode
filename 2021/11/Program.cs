80c103b08ecb753aa34af164291f3f93c7011383
Julien Bara <bara.julien@yahoo.fr>
2021-12-11 11:21:01 +0100

    11-1

---
+++ 2021/11/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+var inputFile = Path.Combine(".", "input");
+
+var input = File
+    .ReadAllLines(inputFile);
+
+var size = input.Length;
+
+var grid = new int[size, size];
+
+for (int i = 0; i < size; i++)
+{
+    for (int j = 0; j < size; j++)
+    {
+        grid[i, j] = Int32.Parse(input[i][j].ToString());
+    }
+}
+
+var flashesCount = 0;
+var greyPositions = new Queue<(int, int)>();
+
+for (int step = 1; step <= 100; step++)
+{
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            grid[i, j]++;
+            if (grid[i, j] > 9)
+                greyPositions.Enqueue((i, j));
+        }
+    }
+
+    while (greyPositions.Count > 0)
+    {
+        var (i, j) = greyPositions.Dequeue();
+
+        if (grid[i, j] == 0) { continue; }
+
+        grid[i, j]++;
+        if (grid[i, j] <= 9) { continue; }
+
+        flashesCount++;
+        grid[i, j] = 0;
+
+        for (int x = Math.Max(0, i - 1); x <= Math.Min(size - 1, i + 1); x++)
+        {
+            for (int y = Math.Max(0, j - 1); y <= Math.Min(size - 1, j + 1); y++)
+            {
+                greyPositions.Enqueue((x, y));
+            }
+        }
+    }
+}
+
+Console.WriteLine(flashesCount);
