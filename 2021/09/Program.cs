988c54646dd6503702032dede8c7336c526f23b1
Julien Bara <bara.julien@yahoo.fr>
2021-12-11 12:17:13 +0100

    9-1

---
+++ 2021/09/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+var inputFile = Path.Combine(".", "input");
+
+var input = File
+    .ReadAllLines(inputFile);
+
+var rowsCount = input.Length;
+var colsCount = input[0].Length;
+
+var grid = new int[rowsCount, colsCount];
+for (int i = 0; i < rowsCount; i++)
+{
+    for (int j = 0; j < colsCount; j++)
+    {
+        grid[i, j] = Int32.Parse(input[i][j].ToString());
+    }
+}
+
+var lowestPointsScore = 0;
+for (int i = 0; i < rowsCount; i++)
+{
+    for (int j = 0; j < colsCount; j++)
+    {
+        if (isLowestPoint(i, j))
+            lowestPointsScore += grid[i, j] + 1;
+    }
+}
+
+Console.WriteLine(lowestPointsScore);
+
+bool isLowestPoint(int i, int j)
+{
+    if (i > 0 && grid[i - 1, j] <= grid[i, j])
+        return false;
+
+    if (j > 0 && grid[i, j - 1] <= grid[i, j])
+        return false;
+
+    if (i < rowsCount - 1 && grid[i + 1, j] <= grid[i, j])
+        return false;
+
+    if (j < colsCount - 1 && grid[i, j + 1] <= grid[i, j])
+        return false;
+
+    return true;
+}
