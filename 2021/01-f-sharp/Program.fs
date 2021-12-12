9ad6c0e91ada96595d2ea043a84c280d3c6ec508
Julien Bara <bara.julien@yahoo.fr>
2021-12-12 15:46:00 +0100

    start 1-1 f#

---
+++ 2021/01-f-sharp/Program.fs
@@ -0,0 +1,10 @@
+﻿// open System.IO
+
+// Path.Combine(".", "input")
+// |> File.ReadAllLines 
+// |> Seq.map (fun line -> line |> int)
+// |> Seq.map (fun n -> [| n; n |])
+// |> Seq.concat
+// |> Seq.skip 1
+// |> Seq.fold 
+// |> printfn "%A"
