0dfa0c77947dd08e050a22f0c375911a32482ada
Julien Bara <bara.julien@yahoo.fr>
2021-12-12 09:12:58 +0100

    12-1

---
+++ 2021/12/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+var inputFile = Path.Combine(".", "input");
+
+var graph = File
+    .ReadAllLines(inputFile)
+    .Select(x => x.Split("-"))
+    .Select(x => new { Start = x[0], End = x[1] })
+    .Aggregate(
+        new Dictionary<string, List<string>>(),
+        (accu, segment) =>
+        {
+            if (!accu.ContainsKey(segment.Start))
+                accu[segment.Start] = new List<string>();
+            if (!accu.ContainsKey(segment.End))
+                accu[segment.End] = new List<string>();
+
+            accu[segment.Start].Add(segment.End);
+            accu[segment.End].Add(segment.Start);
+
+            return accu;
+        }
+    );
+
+var score = countPaths("start", new List<string>());
+
+Console.WriteLine(score);
+
+int countPaths(string currentCave, List<string> visitedSmallCaves)
+{
+    if (currentCave == "end") { return 1; }
+    if (graph[currentCave].All(x => visitedSmallCaves.Contains(x))) { return 0; }
+
+    var count = 0;
+    foreach (var nextCave in graph[currentCave].Where(x => !visitedSmallCaves.Contains(x)))
+    {
+        count += countPaths(
+            nextCave,
+            currentCave.All(x => Char.IsLower(x)) ? visitedSmallCaves.Prepend(currentCave).ToList() : visitedSmallCaves
+        );
+    }
+    return count;
+}
