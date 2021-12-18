b3444c444dbfb2015c43c5e1d935523a56d40f31
Julien Bara <bara.julien@yahoo.fr>
2021-12-18 11:31:48 +0100

    15-2

--- 2021/15/Program.cs
+++ 2021/15/Program.cs
@@ -13,51 +13,78 @@ var input = File.ReadAllLines(inputFile)
     )
     .ToArray();
 
-var riskGrid = Enumerable.Range(0, input.Length)
-    .Select(x => Enumerable.Range(0, input[0].Length)
+var biggerCave = new int[input.Length * 5][];
+
+for (int i = 0; i < 5; i++)
+{
+    for (int j = 0; j < 5; j++)
+    {
+        for (int x = 0; x < input.Length; x++)
+        {
+            biggerCave[i * input.Length + x] = biggerCave[i * input.Length + x] ?? new int[input[0].Length * 5];
+
+            for (int y = 0; y < input[0].Length; y++)
+            {
+                biggerCave[i * input.Length + x][j * input[0].Length + y] = (input[x][y] + i + j);
+                if (biggerCave[i * input.Length + x][j * input[0].Length + y] >= 10)
+                    biggerCave[i * input.Length + x][j * input[0].Length + y] -= 9;
+            }
+        }
+    }
+}
+
+var riskGrid = Enumerable.Range(0, biggerCave.Length)
+    .Select(x => Enumerable.Range(0, biggerCave[0].Length)
         .Select(y => Int32.MaxValue)
         .ToArray()
     ).ToArray();
 
 var processed = new HashSet<(int, int)>();
-riskGrid[input.Length - 1][input[0].Length - 1] = input[input.Length - 1][input[0].Length - 1];
+var toProcess = new HashSet<(int, int)>();
+for (int x = 0; x < riskGrid.Length; x++)
+{
+    for (int y = 0; y < riskGrid.Length; y++)
+    {
+        toProcess.Add((x, y));
+    }
+}
 
-while (processed.Count < input.Length * input[0].Length)
+riskGrid[biggerCave.Length - 1][biggerCave[0].Length - 1] = biggerCave[biggerCave.Length - 1][biggerCave[0].Length - 1];
+
+while (processed.Count < biggerCave.Length * biggerCave[0].Length)
 {
     var (x, y) = itemToProcess();
 
     if (y - 1 >= 0)
-        riskGrid[x][y - 1] = Math.Min(riskGrid[x][y - 1], riskGrid[x][y] + input[x][y - 1]);
+        riskGrid[x][y - 1] = Math.Min(riskGrid[x][y - 1], riskGrid[x][y] + biggerCave[x][y - 1]);
 
-    if (y + 1 < input[0].Length)
-        riskGrid[x][y + 1] = Math.Min(riskGrid[x][y + 1], riskGrid[x][y] + input[x][y + 1]);
+    if (y + 1 < biggerCave[0].Length)
+        riskGrid[x][y + 1] = Math.Min(riskGrid[x][y + 1], riskGrid[x][y] + biggerCave[x][y + 1]);
 
     if (x - 1 >= 0)
-        riskGrid[x - 1][y] = Math.Min(riskGrid[x - 1][y], riskGrid[x][y] + input[x - 1][y]);
+        riskGrid[x - 1][y] = Math.Min(riskGrid[x - 1][y], riskGrid[x][y] + biggerCave[x - 1][y]);
 
-    if (x + 1 < input.Length)
-        riskGrid[x + 1][y] = Math.Min(riskGrid[x + 1][y], riskGrid[x][y] + input[x + 1][y]);
+    if (x + 1 < biggerCave.Length)
+        riskGrid[x + 1][y] = Math.Min(riskGrid[x + 1][y], riskGrid[x][y] + biggerCave[x + 1][y]);
 
     processed.Add((x, y));
+    toProcess.Remove((x, y));
+    Console.Write("\rProgress: {0}%   ", 100 * processed.Count / (500 * 500));
 }
 
-Console.WriteLine(riskGrid[0][0] - input[0][0]);
+Console.WriteLine(riskGrid[0][0] - biggerCave[0][0]);
 
 (int, int) itemToProcess()
 {
     var minValue = Int32.MaxValue;
     var minIndex = (0, 0);
 
-    for (int x = 0; x < input.Length; x++)
+    foreach (var (x, y) in toProcess)
     {
-        for (int y = 0; y < input[0].Length; y++)
-        {
-            if (processed.Contains((x, y))) { continue; }
-            if (riskGrid[x][y] >= minValue) { continue; }
+        if (riskGrid[x][y] >= minValue) { continue; }
 
-            minValue = riskGrid[x][y];
-            minIndex = (x, y);
-        }
+        minValue = riskGrid[x][y];
+        minIndex = (x, y);
     }
 
     return minIndex;
