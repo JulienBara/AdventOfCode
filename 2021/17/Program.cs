7e73bbc25ed0984be370173224c7ceaa3aec415e
Julien Bara <bara.julien@yahoo.fr>
2021-12-18 15:42:44 +0100

    17-2

--- 2021/17/Program.cs
+++ 2021/17/Program.cs
@@ -7,7 +7,7 @@ using System.Text.RegularExpressions;
 var inputFile = Path.Combine(".", "input");
 
 var input = File.ReadAllText(inputFile);
-var regexMatches = new Regex(@"target area: x=(?<xMin>-?\d+?)\.\.(?<xMax>-?\d+?), y=(?<yMin>-?\d+?)\.\.(?<yMax>-?\d+?)")
+var regexMatches = new Regex(@"^target area: x=(?<xMin>-?\d+?)\.\.(?<xMax>-?\d+?), y=(?<yMin>-?\d+?)\.\.(?<yMax>-?\d+?)$")
     .Match(input)
     .Groups;
 var xMin = Int32.Parse(regexMatches["xMin"].Value);
@@ -15,38 +15,37 @@ var xMax = Int32.Parse(regexMatches["xMax"].Value);
 var yMin = Int32.Parse(regexMatches["yMin"].Value);
 var yMax = Int32.Parse(regexMatches["yMax"].Value);
 
-// (xInitialVelocity, yInitialVelocity, highestY)
-var validInitialVelocities = new List<(int, int, int)>();
+// (xInitialVelocity, yInitialVelocity)
+var validInitialVelocities = new List<(int, int)>();
 
-for (int xInitialVelocity = -1000; xInitialVelocity < 1000; xInitialVelocity++)
+for (int xInitialVelocity = 0; xInitialVelocity < 100; xInitialVelocity++)
 {
     for (int yInitialVelocity = -1000; yInitialVelocity < 1000; yInitialVelocity++)
     {
         var x = 0;
         var y = 0;
-        var highestY = 0;
         var xVelocity = xInitialVelocity;
         var yVelocity = yInitialVelocity;
         while (x <= xMax && y >= yMin)
         {
+            x += xVelocity;
+            y += yVelocity;
+            xVelocity += xVelocity > 0 ? -1 : xVelocity < 0 ? 1 : 0;
+            yVelocity += -1;
+
             if (xMin <= x
             && x <= xMax
             && yMin <= y
             && y <= yMax)
-                validInitialVelocities.Add((xInitialVelocity, yInitialVelocity, highestY));
-
-            x += xVelocity;
-            y += yVelocity;
-            highestY = Math.Max(highestY, y);
-            xVelocity += xVelocity > 0 ? -1 : xVelocity < 0 ? 1 : 0;
-            yVelocity += -1;
+            {
+                validInitialVelocities.Add((xInitialVelocity, yInitialVelocity));
+                break;
+            }
         }
     }
 }
 
 var result = validInitialVelocities
-    .OrderByDescending(x => x.Item3)
-    .First()
-    .Item3;
+    .Count();
 
 Console.WriteLine(result);
