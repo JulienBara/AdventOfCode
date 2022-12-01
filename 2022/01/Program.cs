43a60277b68004f1141b5898d7cb1ca3878dafe3
julien <julien.bara@360learning.com>
2022-12-01 07:16:53 +0100

    2022-1

---
+++ 2022/01/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+var inputFile = Path.Combine(".", "input");
+var result = File
+    .ReadAllText(inputFile)
+    .Split(Environment.NewLine + Environment.NewLine)
+    .Select(x => x.Split(Environment.NewLine))
+    .Select(x => x.Select(y => Int32.Parse(y)))
+    .Select(x => x.Sum())
+    .OrderByDescending(x => x)
+    .First();
+
+System.Console.WriteLine(result);
+
