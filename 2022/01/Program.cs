37876915485e12a5cfaf5d281ab86cd9b3a6995d
julien <julien.bara@360learning.com>
2022-12-01 07:18:08 +0100

    2022-2

--- 2022/01/Program.cs
+++ 2022/01/Program.cs
@@ -4,7 +4,8 @@ using System.IO;
 using System.Linq;
 
 var inputFile = Path.Combine(".", "input");
-var result = File
+
+var result1 = File
     .ReadAllText(inputFile)
     .Split(Environment.NewLine + Environment.NewLine)
     .Select(x => x.Split(Environment.NewLine))
@@ -13,5 +14,17 @@ var result = File
     .OrderByDescending(x => x)
     .First();
 
-System.Console.WriteLine(result);
+System.Console.WriteLine(result1);
+
+var result2 = File
+    .ReadAllText(inputFile)
+    .Split(Environment.NewLine + Environment.NewLine)
+    .Select(x => x.Split(Environment.NewLine))
+    .Select(x => x.Select(y => Int32.Parse(y)))
+    .Select(x => x.Sum())
+    .OrderByDescending(x => x)
+    .Take(3)
+    .Sum();
+
+System.Console.WriteLine(result2);
 
