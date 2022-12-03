0f924a5d9e45abb18bed456d9c310191420c1618
julien <julien.bara@360learning.com>
2022-12-03 15:25:27 +0100

    03-01

---
+++ 2022/03/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+var inputFile = Path.Combine(".", "input");
+
+var result1 = File
+    .ReadAllLines(inputFile)
+    .Select(x => (
+        x.Take(x.Length / 2),
+        x.Skip(x.Length / 2)
+    ))
+    .Select(x => {
+        foreach (var item in x.Item2)
+        {
+            if(x.Item1.Contains(item)) {
+                return item.ToString();
+            }
+        }
+
+        throw new Exception("should not happen");
+    })
+    .Select(x => Encoding.ASCII.GetBytes(x)[0])
+    .Select(x => 
+        x > 96 ?
+            x - 96 : // 'a' = 97
+            26 + x - 64 // 'A' = 65
+    )
+    .Sum();
+
+System.Console.WriteLine(result1);
