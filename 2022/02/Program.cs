3f9d0e4608f4f15a58f1ff40e2a815a7010d9fc7
julien <julien.bara@360learning.com>
2022-12-03 14:17:17 +0100

    02-01

---
+++ 2022/02/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+var inputFile = Path.Combine(".", "input");
+
+var result1 = File
+    .ReadAllLines(inputFile)
+    .Select(x => x.Split())
+    .Select(x => {
+        if (x[0] == "A" && x[1] == "X") { return 1 + 3; }
+        if (x[0] == "A" && x[1] == "Y") { return 2 + 6; }
+        if (x[0] == "A" && x[1] == "Z") { return 3 + 0; }
+
+        if (x[0] == "B" && x[1] == "X") { return 1 + 0; }
+        if (x[0] == "B" && x[1] == "Y") { return 2 + 3; }
+        if (x[0] == "B" && x[1] == "Z") { return 3 + 6; }
+
+        if (x[0] == "C" && x[1] == "X") { return 1 + 6; }
+        if (x[0] == "C" && x[1] == "Y") { return 2 + 0; }
+        if (x[0] == "C" && x[1] == "Z") { return 3 + 3; }
+
+        throw new Exception("should not happen");
+    })
+    .Sum();
+
+System.Console.WriteLine(result1);
